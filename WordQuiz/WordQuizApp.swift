101b34ded3e564192be856e20b4460cb1d0be9f2
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-01-21 20:53:35 +0700

    initialized project

---
+++ WordQuiz/WordQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordQuizApp.swift
+//  WordQuiz
+//
+//  Created by Pakanon Pantisawat on 21/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct WordQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordQuizTests/WordQuizTests.swift
@@ -0,0 +1,33 @@
+//
+//  WordQuizTests.swift
+//  WordQuizTests
+//
+//  Created by Pakanon Pantisawat on 21/1/2564 BE.
+//
+
+import XCTest
+@testable import WordQuiz
+
+class WordQuizTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
